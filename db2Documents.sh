82e2f6515d22d46d7af6a194161c53ce09f309a2
phdenzel <phdenzel@gmail.com>
2017-06-13 15:11:00 +0200

    Second Dropbox linking script

--- db2Documents.sh
+++ db2Documents.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # link directories to ~/Documents
-for folder in ~/Dropbox/{cheat-sheets,cv,finances,manuals,tolino,travel-docs}; do
+echo "Directory links"
+for folder in ~/Dropbox/{cheat-sheets,conferences,configs,cv,finances,lectures,manuals,master-thesis,papers,personal,presentations,research,scores,teaching,tolino,travel-docs}; do
     dname=${folder##*/}
     if [ -e "$HOME/Documents/$dname" ]; then
-        echo "Link ~Documents/$dname does already exist"
+        echo "    Link ~Documents/$dname does already exist"
     else
-        echo "Linking ~Dropbox/$dname to ~Documents/"
+        echo "    Linking ~Dropbox/$dname to ~Documents/"
         ln -s $folder ~/Documents/$dname
     fi;
 done;
@@ -14,21 +15,21 @@ unset folder;
 unset dname;
 
 # exception when only contents should be linked to ~/Documents
+echo "Content links"
 for folder in ~/Dropbox/family; do
     dname=${folder##*/}
     if [ -d "$HOME/Documents/$dname" ]; then
-        echo "Directory ~/Documents/$dname already exists"
+        echo "    Directory ~/Documents/$dname already exists"
     else
-        echo "Making a new ~/Documents/$dname directory"
+        echo "    Making a new ~/Documents/$dname directory"
         mkdir $HOME/Documents/$dname
     fi;
     for content in $folder/*; do
         cname=${content##*/}
-        echo $HOME/Documents/$dname/$cname
         if [ -e "$HOME/Documents/$dname/$cname" ]; then
-            echo "Link ~Documents/$dname/$cname does already exist"
+            echo "    Link ~Documents/$dname/$cname does already exist"
         else
-            echo "Linking ~Dropbox/$dname/$cname to ~Documents/$dname/"
+            echo "    Linking ~Dropbox/$dname/$cname to ~Documents/$dname/"
             ln -s $HOME/Dropbox/$dname/$cname $HOME/Documents/$dname/$cname
         fi;
     done;
---
+++ db2Home.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# link directories to ~/
+echo "Directory links"
+for folder in ~/Dropbox/{papers,research,teaching}; do
+    dname=${folder##*/}
+    if [ -e "$HOME/$dname" ]; then
+        echo "    Link ~/$dname does already exist"
+    else
+        echo "    Linking ~Dropbox/$dname to ~/"
+        ln -s $folder ~/$dname
+    fi;
+done;
+unset folder;
+unset dname;
+
+# exception when only contents should be linked to ~/
+echo "Content links"
+for folder in ~/Dropbox/lensing; do
+    dname=${folder##*/}
+    # if [ -d "$HOME/$dname" ]; then
+    #     echo "Directory ~/$dname already exists"
+    # else
+    #     echo "Making a new ~/$dname directory"
+    #     mkdir $HOME/$dname
+    # fi;
+    for content in $folder/*; do
+        cname=${content##*/}
+        if [ -e "$HOME/$cname" ]; then
+            echo "    Link ~/$cname does already exist"
+        else
+            echo "    Linking ~Dropbox/$dname/$cname to ~/"
+            ln -s $HOME/Dropbox/$dname/$cname $HOME/$cname
+        fi;
+    done;
+done;
+unset folder;
+unset content;
+unset dname;
+unset cname;

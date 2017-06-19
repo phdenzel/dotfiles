5d971bcb815a1d6b70c8f468b94ab107ef3e5175
phdenzel <phdenzel@gmail.com>
2017-06-19 15:10:20 +0200

    Updated db-linkers

--- db2Documents.sh
+++ db2Documents.sh
@@ -2,7 +2,7 @@
 
 # link directories to ~/Documents
 echo "Directory links"
-for folder in ~/Dropbox/{cheat-sheets,conferences,configs,cv,finances,icons,lectures,manuals,master-thesis,papers,personal,presentations,research,scores,software,teaching,tolino,travel-docs}; do
+for folder in ~/Dropbox/{cheat-sheets,conferences,configs,cv,finances,how-tos,icons,lectures,manuals,master-thesis,papers,personal,presentations,research,scores,software,teaching,tolino,travel-docs}; do
     dname=${folder##*/}
     if [ -e "$HOME/Documents/$dname" ]; then
         echo "    Link ~/Documents/$dname does already exist"

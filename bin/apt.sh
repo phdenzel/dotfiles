579af8d06ee7c69690225c9cefa3f146e476a833
phdenzel <phdenzel@gmail.com>
2017-10-12 17:27:18 +0200

    Emacs install preparation for windows-ubuntu layer

--- bin/apt.sh
+++ bin/apt.sh
@@ -4,5 +4,32 @@
 # Essentials
 sudo apt-get install linuxbrew-wrapper
 sudo apt-get install ssh
+sudo apt-get install wget
 # System utilities
 sudo apt-get install sysstat
+# Dev utilities
+sudo apt-get install autoconf
+sudo apt-get install build-essential checkinstall
+sudo apt-get install gccgo-6
+sudo apt-get install gnutls-bin
+sudo apt-get install libgnutls-dev
+sudo apt-get install libx11-dev
+sudo apt-get install libjpeg-dev
+sudo apt-get install libgif-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install libncurses5-dev
+sudo apt-get install libxft-dev
+sudo apt-get install librsvg2-dev
+sudo apt-get install libmagickcore-dev
+sudo apt-get install libmagick++-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install libgpm-dev
+sudo apt-get install libotf-dev
+sudo apt-get install libm17n-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libwebkitgtk-3.0-dev
+sudo apt-get install libxpm-dev
+sudo apt-get install libxaw7-dev
+# Misc
+sudo apt-get install imagemagick
+

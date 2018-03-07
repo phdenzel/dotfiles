af965f0107d5d34a22607e78e426412a4c6bc9dd
phdenzel <phdenzel@gmail.com>
2018-03-07 14:25:29 +0100

    Some adjustments for emacs compilation

--- bin/emacs_install.sh
+++ bin/emacs_install.sh
@@ -4,7 +4,13 @@ cd ~/local/
 git clone git://git.savannah.gnu.org/emacs.git
 cd emacs/
 ./autogen.sh
-CC=/usr/bin/clang ./configure --with-ns --without-makeinfo
-make install
-cd nextstep/
-cp -r Emacs.app /Applications/
+if [ $OS == "Darwin" ]; then
+    CC=/usr/bin/clang ./configure --with-ns --without-makeinfo
+    make && make install
+    cd nextstep/
+    cp -r Emacs.app /Applications/
+elif [ $OS == "Linux" ]; then
+    ./configure --prefix=$HOME/local/emacs --with-gif=no --with-gnutls=no
+    make && make install
+    ln -s $HOME/local/emacs/bin/emacs $HOME/local/bin/emacs
+else

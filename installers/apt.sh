839d274d26d0c74c885a7227da58982fab7453b9
phdenzel <phdenzel@gmail.com>
2018-05-16 11:10:00 +0200

    Updated apt installer

--- installers/apt.sh
+++ installers/apt.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 ####################################################### Homebrew installs
+# Remove non-essentials
+sudo apt-get remove --purge gnome-games
+sudo apt-get autoremove
 # Install apt packages
 # Essentials
-sudo apt-get install linuxbrew-wrapper
-sudo apt-get install ssh
-sudo apt-get install x11-apps
+sudo apt-get install gnome-core        # GNOME desktop manager
 sudo apt-get install gnome-calculator  # for gtk
+sudo apt-get install git               # very likely already installed
+sudo apt-get install ssh
 sudo apt-get install wget
+sudo apt-get install x11-apps
 # System utilities
 sudo apt-get install sysstat
 # Dev utilities
 sudo apt-get install autoconf
-sudo apt-get install build-essential checkinstall
-sudo apt-get install gccgo-6
+sudo apt-get install gcc
+sudo apt-get install build-essential
+sudo apt-get install checkinstall
 sudo apt-get install gnutls-bin
+sudo apt-get install python python3
+sudo apt-get install ruby ruby-dev
+# Libs
 sudo apt-get install libgnutls-dev
 sudo apt-get install libx11-dev
 sudo apt-get install libjpeg-dev
@@ -32,8 +40,6 @@ sudo apt-get install libgtk-3-dev
 sudo apt-get install libwebkitgtk-3.0-dev
 sudo apt-get install libxpm-dev
 sudo apt-get install libxaw7-dev
-sudo apt-get install ruby
-sudo apt-get install ruby-dev
+
 # Misc
 sudo apt-get install imagemagick
-sudo apt-get install python-tk

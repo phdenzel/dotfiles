bed0f98e95272c382fc08d8df40d3b38effb409c
phdenzel <phdenzel@gmail.com>
2022-01-14 00:19:33 +0100

    Moved compatible files into xdg_config_home

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,11 @@
 #                --gtk        : install custom gtk theme
 BOOTSTRAP_PATH=$(dirname "$0")
 BOOTSTRAP_PATH=$(cd "$BOOTSTRAP_PATH" && pwd)
+CONF_HOME=${XDG_CONFIG_HOME:=$HOME/.config}
+echo "$BOOTSTRAP_PATH"
 EXCLUDES=(
     --exclude ".git/"
     --exclude ".gitmodules"
-    --exclude ".macOS"
     --exclude "bootstrap.sh"
     --exclude "bin/"
     --exclude "installers/"
@@ -22,37 +23,41 @@ EXCLUDES=(
     --exclude "imgs/"
     --exclude "utils/"
     --exclude "private/"
-    --exclude ".emacs.d/"
+    --exclude ".config/emacs/"
+    --exclude ".config/gtk-3.0/"
+    --exclude ".config/xmobar/xmobarconf/"
     --exclude "LICENSE"
     --exclude "README.md"
-    --exclude "phd-dark.tmTheme"
-    --exclude ".config/gtk-3.0/"
-    --exclude ".config/xmobar/xmobarconf/.git"
-    #--exclude ".editorconfig"
+    --exclude ".themes/phd-dark.tmTheme"
+    --exclude ".editorconfig"
 )
 
 if [ "$1" == "--emacs" ]; then
     # Copy .emacs.d to its rightful place                                 
-    if [ ! -d "${HOME}/.emacs.d/" ]; then
-	      cp -r .emacs.d ${HOME}/
+    if [ ! -d "$CONF_HOME/emacs" ]; then
+	      cp -r .config/emacs $CONF_HOME/emacs
     else
-        mv ${HOME}/.emacs.d ${HOME}/emacs.d.bak
-	      cp -r .emacs.d ${HOME}/
-	      echo ".emacs.d already existed and has been backed up to ~/emacs.d.bak"
+        mv $CONF_HOME/emacs $CONF_HOME/emacs.bak
+	      cp -r .config/emacs $CONF_HOME/
+	      echo ".config/emacs directory already existed and has been backed up to .config/emacs.bak"
     fi;
 elif [ "$1" == "--emacs-sync" ]; then
-    rsync -ahv .emacs.d/ ~/.emacs.d/
+    rsync -ahv .config/emacs/ $CONF_HOME/emacs/
 elif [ "$1" == "--bin" ]; then
     # Link the binaries to ~/local/bin/
     mkdir -p ${HOME}/local/bin/  # don't forget to add to PATH
-    ln -s $(pwd)/bin/* ${HOME}/local/bin/
+    ln -s $BOOTSTRAP_PATH/bin/* ${HOME}/local/bin/
 elif [ "$1" == "--gtk" ]; then
-    rsync -ahv .config/gtk-3.0/ ~/.config/gtk-3.0/
+    rsync -ahv .config/gtk-3.0/ $CONF_HOME/gtk-3.0/
 elif [ "$1" == "--dry-run" ]; then
     rsync "${EXCLUDES[@]}" --dry-run -avh . ~;
     exit 1
 else
     rsync "${EXCLUDES[@]}" -avh . ~;
+    git submodule update
+    if [ ! -d $CONF_HOME/xmobar/xmobarconf ]; then
+        ln -s $BOOTSTRAP_PATH/.config/xmobar/xmobarconf $CONF_HOME/xmobar/xmobarconf
+    fi;
 fi;
 
 source ~/.bashrc

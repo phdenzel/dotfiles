a3b87a2583d1ab34b0283cdbcf5b9eff99703a66
phdenzel <phdenzel@gmail.com>
2022-01-24 09:24:24 +0100

    Fix palette for colors 00-15

--- utils/palette.sh
+++ utils/palette.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ### Main color palette
-for i in 232 255 160 29 222 27 125 44 215 105 239;
+for i in 1 2 3 4 5 6 7 8;
 do
     printf "\e[1;38;5;${i}m%03d" $i ;
     printf '\e[0m';
 done
 echo
 ### Bright colors
-for i in 232 255 161 35 220 19 198 51 202 57 236;
+for i in 9 10 11 12 13 14 15 16;
 do
     printf "\e[1;38;5;${i}m%03d" $i ;
     printf '\e[0m';

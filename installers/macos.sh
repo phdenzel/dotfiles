cecccabbd5da368a1c205d7e0dc037a56e8ca453
phdenzel <phdenzel@gmail.com>
2023-07-31 22:17:10 +0200

    Cleaned up outdated install scripts

--- installers/macos.sh
+++ installers/macos.sh
@@ -8,12 +8,12 @@ sudo mdutil -i off -a
 mkdir -p ${HOME}/.gnupg && chmod 700 ${HOME}/.gnupg
 mkdir -p ${HOME}/.ssh && chmod 700 ${HOME}/.ssh
 if [ ! -f "${HOME}/.ssh/id_ed25519" ]; then
-	ssh-keygen -t ed25519 -q -f "${HOME}/.ssh/id_ed25519" -C "$EMAIL" -N "";
+	  ssh-keygen -t ed25519 -q -f "${HOME}/.ssh/id_ed25519" -C "$EMAIL" -N "";
 fi;
 if [ ! -f "${HOME}/.ssh/gh_id_ed25519" ]; then
-	ssh-keygen -t ed25519 -q -f "${HOME}/.ssh/gh_id_ed25519" -C "$EMAIL" -N "";
-	echo -e "Please register the generated ssh key ${HOME}/.ssh/gh_id_ed25519.pub \n\t$(cat ${HOME}/.ssh/gh_id_ed25519.pub) \n with your GitHub account at https://github.com/settings/keys and re-run this script..."
-	exit 0;
+	  ssh-keygen -t ed25519 -q -f "${HOME}/.ssh/gh_id_ed25519" -C "$EMAIL" -N "";
+	  echo -e "Please register the generated ssh key ${HOME}/.ssh/gh_id_ed25519.pub \n\t$(cat ${HOME}/.ssh/gh_id_ed25519.pub) \n with your GitHub account at https://github.com/settings/keys and re-run this script..."
+	  exit 0;
 fi;
 
 ### dotfiles
@@ -24,8 +24,8 @@ cd $HOME/dotfiles && ./bootstrap.sh -b -e
 
 ### Homebrew install
 if [ ! -d "/opt/homebrew" ]; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-	PATH=/opt/homebrew/bin:$PATH;
+	  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+	  PATH=/opt/homebrew/bin:$PATH;
 fi;
 
 brew update
@@ -33,4 +33,3 @@ brew install autoconf bat binutils colordiff coreutils diffutils emacs findutils
 # casks
 brew tap homebrew/cask-fonts
 brew install --cask alacritty alfred brave-browser emacs font-fira-code font-fira-mono font-fira-sans
-

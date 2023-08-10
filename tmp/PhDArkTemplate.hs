618da25c38bb980ef52e7ab7869a2b4bac29da97
phdenzel <phdenzel@gmail.com>
2023-08-10 17:09:52 +0200

    More theme templates + svg cicle generator

---
+++ tmp/PhDArkTemplate.hs
@@ -0,0 +1,38 @@
+module Colors.#phd-ark-pascalcase where
+
+colorScheme :: String
+colorScheme = "#phd-ark-title"
+
+colorBack, colorFore :: String
+colorBack = "#phd-ark-base"
+colorFore = "#phd-ark-subtext1"
+
+color00, color01, color02, color03, color04, color05, color06, color07 :: String
+color08, color09, color10, color11, color12, color13, color14, color15 :: String
+-- Black
+color00 = "#phd-ark-overlay2"
+color08 = "#phd-ark-overlay1"
+-- Red
+color01 = "#phd-ark-red"
+color09 = "#phd-ark-ruby"
+-- Green
+color02 = "#phd-ark-green"
+color10 = "#phd-ark-viridis"
+-- Yellow
+color03 = "#phd-ark-yellow"
+color11 = "#phd-ark-sand"
+-- Blue
+color04 = "#phd-ark-blue"
+color12 = "#phd-ark-indigo"
+-- Magenta
+color05 = "#phd-ark-pink"
+color13 = "#phd-ark-magenta"
+-- Cyan
+color06 = "#phd-ark-turquoise"
+color14 = "#phd-ark-cyan"
+-- White
+color07 = "#phd-ark-subtext0"
+color15 = "#phd-ark-white"
+
+colorTrayer :: String
+colorTrayer = "--tint #phd-ark-hexbase "
--- tmp/generate_scheme.py
+++ tmp/generate_scheme.py
@@ -8,15 +8,23 @@ from phd_ark import palettes, all_palettes
 
 flavors = list(palettes.keys())
 
+SVG_CIRCLE="""
+<svg height="50" width="50">
+  <circle cx="25" cy="25" r="20" stroke="{color}" stroke-width="3" fill="{color}" />
+</svg>
+"""
+
 
 def parse_args():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    reqarg = parser.add_mutually_exclusive_group(required=True)
+    reqarg.add_argument('-i', '--template', '--input', '--file', type=Path,
+                        help="Input template file to generate scheme.")
+    reqarg.add_argument('--palette', action='store_true',
+                        help="Generate palette as svg circles.")
     parser.add_argument('-f', '--flavor', '--flavour',
                         type=str, choices=['all']+flavors,
                         help="Select the phd-ark color scheme flavor.")
-    parser.add_argument('-i', '--template', '--input', '--file',
-                        type=Path, required=True,
-                        help="Input template file to generate scheme.")
     parser.add_argument('-o', '--output', '--scheme',
                         type=Path,
                         help="Output file for the generated scheme.")
@@ -30,20 +38,10 @@ def generate(args, search_prefix='#phd-ark-'):
     """
     Generate a theme from a template
     """
-    # get template file content
-    tmpl = args.template.resolve()
-    if args.output is None:
-        filename = Path(
-            f"{str(tmpl.stem).replace('-template', '')}"
-            f"-{args.flavor}"
-            f"{tmpl.suffix}")
-    else:
-        filename = args.output.resolve()
-    content = tmpl.read_text()
     # deduce flavor if necessary
     flavor = args.flavor
     if args.flavor is None and args.output is not None:
-        for f in flavors:
+        for f in flavors + ['all']:
             if f in str(args.output):
                 flavor = f
                 break
@@ -54,6 +52,16 @@ def generate(args, search_prefix='#phd-ark-'):
         colors = palettes[flavor]
     else:
         colors = all_palettes(palettes)
+    # get template file content
+    tmpl = args.template.resolve()
+    if args.output is None:
+        filename = Path(
+            f"{str(tmpl.stem).replace('-template', '')}"
+            f"-{args.flavor}"
+            f"{tmpl.suffix}")
+    else:
+        filename = args.output.resolve()
+    content = tmpl.read_text()
     # sort keys to ensure /b256 colors are used first
     color_keys = sorted(colors.keys(),
                         key=lambda x: x.endswith('/b256'),
@@ -70,6 +78,55 @@ def generate(args, search_prefix='#phd-ark-'):
     else:
         print(content)
 
+
+def svg_palette(args,
+                svg_template=SVG_CIRCLE.strip(),
+                default_outdir='phd-ark'):
+    """
+    Generate SVG palettes of a theme
+    """
+    # deduce flavor if necessary
+    flavor = args.flavor
+    if args.flavor is None and args.output is not None:
+        for f in flavors + ['all']:
+            if f in str(args.output):
+                flavor = f
+                break
+    if flavor is None:
+        flavor = 'all'
+    flavor_keys = flavors if flavor == 'all' else [flavor]
+    for f in flavor_keys:
+        colors = palettes[f]
+        # sort keys to ensure /b256 colors are used first
+        color_keys = sorted(colors.keys(),
+                            key=lambda x: x.endswith('/b256'),
+                            reverse=True)
+        # determine output directory
+        if args.output is None:
+            outdir = Path(f"{default_outdir}-{f}")
+        else:
+            outdir = args.output
+        if f not in str(outdir):
+            outdir = Path(str(outdir) + f"-{f}")
+        print(outdir)
+        # generate a file for each color
+        for cname in color_keys:
+            color = colors[cname]
+            if not isinstance(color, str) \
+               or not color.startswith('#') or cname.endswith('/b256'):
+                continue
+            content = svg_template.format(color=color)
+            filename = outdir / Path(f"{cname}.svg")
+            if args.save or args.output is not None:
+                filename.write_text(content)
+            else:
+                print(f"{filename}:")
+                print(content, end="\n\n")
+
+
 if __name__ == "__main__":
     args = parse_args()
-    generate(args)
+    if args.palette:
+        svg_palette(args)
+    else:
+        generate(args)

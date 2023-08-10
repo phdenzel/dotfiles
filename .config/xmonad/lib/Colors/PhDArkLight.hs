5fed5b04b0d3e59383292c03a6c045f1447529eb
phdenzel <phdenzel@gmail.com>
2023-08-10 23:22:08 +0200

    Updated themes generated from templates

---
+++ .config/xmonad/lib/Colors/PhDArkLight.hs
@@ -0,0 +1,38 @@
+module Colors.PhDArkDefault where
+
+colorScheme :: String
+colorScheme = "phd-ark-default"
+
+colorBack, colorFore :: String
+colorBack = "#303033"
+colorFore = "#DDE3EE"
+
+color00, color01, color02, color03, color04, color05, color06, color07 :: String
+color08, color09, color10, color11, color12, color13, color14, color15 :: String
+-- Black
+color00 = "#8A8A8A"
+color08 = "#464858"
+-- Red
+color01 = "#FF6D6B"
+color09 = "#D7005F"
+-- Green
+color02 = "#44BC84"
+color10 = "#00AF87"
+-- Yellow
+color03 = "#FFD787"
+color11 = "#FDB760"
+-- Blue
+color04 = "#5F8AF7"
+color12 = "#5F5FFF"
+-- Magenta
+color05 = "#E83A82"
+color13 = "#FF0087"
+-- Cyan
+color06 = "#1AE6CC"
+color14 = "#46D9FF"
+-- White
+color07 = "#EDF3FE"
+color15 = "#F6F9FE"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x303033 "

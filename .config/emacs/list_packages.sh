bf510be9f705275ac7557647a8d0d0ca3647240c
phdenzel <phdenzel@gmail.com>
2024-03-13 01:15:30 +0100

    emacs python lsp upgrade

--- .config/emacs/list_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-if [[ "$1" == "-sort" || "$1" == "--sort" || "$1" == "-s" ]]; then
-    grep "(use-package" "$2" | awk -F 'use-package' '{print $2}' | sort;
-else
-    grep "(use-package" "$1" | awk -F 'use-package' '{print $2}';
-fi;

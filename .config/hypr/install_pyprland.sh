27fc7e9d280a872bea30e52b9b4d531f29e6d658
phdenzel <phdenzel@gmail.com>
2024-10-27 17:05:36 +0100

    Added hyprland scratchpads with pypr + minor changes

---
+++ .config/hypr/install_pyprland.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/hyprland-community/pyprland/main/scripts/get-pypr | sh
---
+++ .config/hypr/uninstall_pyprland.sh
@@ -0,0 +1,1 @@
+sudo rm -fr /var/cache/pypr /usr/local/bin/pypr ; rm ~/.config/hypr/pyprland.toml
